e3969f6d863d364df0d0c493d8c0c04e66a04c97
Melissa Marentette <melrosemarentette@gmail.com>
2024-09-10 23:50:22 -0400

    All green tests

---
+++ ruby/savings-account/savings_account.rb
@@ -0,0 +1,29 @@
+module SavingsAccount
+  def self.years_before_desired_balance(balance, desired_balance)
+    years_passed = 0
+
+    until balance >= desired_balance
+      balance = annual_balance_update(balance)
+      years_passed += 1
+    end
+
+    years_passed
+  end
+
+  def self.annual_balance_update(balance)
+   balance + (balance.abs * interest_rate(balance) / 100)
+  end
+
+  def self.interest_rate(balance)
+    case balance
+    when ...0
+      -3.213
+    when 0...1000
+      0.5
+    when 1000...5000
+      1.621
+    when 5000...1000000000
+      2.475
+    end
+  end
+end
