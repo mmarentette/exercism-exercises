758f088742d1407f4ba2be57cbb7096dce0776ff
Melissa Marentette <melrosemarentette@gmail.com>
2024-09-12 13:47:20 -0400

    Add extend self

--- ruby/port-palermo/port_palermo.rb
+++ ruby/port-palermo/port_palermo.rb
@@ -1,11 +1,13 @@
 module Port
+  extend self
+  
   IDENTIFIER = :PALE
 
-  def self.get_identifier(port)
+  def get_identifier(port)
     port[..3].upcase.to_sym
   end
 
-  def self.get_terminal(ship_identifier)
+  def get_terminal(ship_identifier)
     cargo = ship_identifier[..2]
 
     ["OIL", "GAS"].include?(cargo) ? :A : :B
