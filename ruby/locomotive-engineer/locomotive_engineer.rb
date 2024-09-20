b23efb4618f0042f07cad0fea50ebc53ca0091a0
Melissa Marentette <melrosemarentette@gmail.com>
2024-09-19 23:55:29 -0400

    Implement .generate_list_of_wagons and .fix_list_of_wagons methods

--- ruby/locomotive-engineer/locomotive_engineer.rb
+++ ruby/locomotive-engineer/locomotive_engineer.rb
@@ -1,15 +1,9 @@
 class LocomotiveEngineer
-  def self.generate_list_of_wagons(*wagon_ids)= wagon_ids
-
-  def self.fix_list_of_wagons((penultimate, last, first, *rest), missing_wagons)
-    [first, *missing_wagons, *rest, penultimate, last]
+  def self.generate_list_of_wagons(*wagon_ids)
+    wagon_ids
   end
 
-  def self.add_missing_stops(route, **stops)
-    {**route, stops: stops.values}
-  end
-
-  def self.extend_route_information(route, more_route_info)
-    {**route, **more_route_info}
+  def self.fix_list_of_wagons((penultimate, last, first, *rest), second_list)
+    fixed_list = first, *second_list, *rest, penultimate, last
   end
 end
